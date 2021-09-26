08852fbe5b7e7ffe398e73cdbb3eb7fa2f42dc95
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-09-25 23:17:17 -0400

    Calcular el descuento

--- src/App.test.js
+++ src/App.test.js
@@ -19,9 +19,26 @@ function calcularTotal(cantidad, precio, estado=""){
   }
   var total;
   var totalConImpuesto;
+  var totalConDescuento;
   total = cantidad* precio;
   totalConImpuesto = total+total*impuestoEstado
-  return "El total es: "+total+", el impuesto del estado "+estado+" es: "+impuestoEstado+" y el precio con impuesto es: "+totalConImpuesto;
+  var descuentoPrecio = 0;
+    if (total >= 1000){
+        descuentoPrecio = 0.03;
+    }else if(total >= 3000){
+        descuentoPrecio = 0.05;
+    }else if(total >= 7000){
+        descuentoPrecio = 0.07;
+    }else if(total >= 10000){
+        descuentoPrecio = 0.1;
+    }else if(total >= 30000){
+        descuentoPrecio = 0.15;
+    }
+  return "El total es: "+total+
+          ", el impuesto del estado "+estado+
+          " es: "+impuestoEstado+
+          ", el precio con impuesto es: "+totalConImpuesto+
+          " y usted tiene un descuento de: "+descuentoPrecio;
 }
 //Primer commit, mandamos un precio y una cantidad y calcula el total
 /*describe("calcular", () => {
@@ -36,10 +53,14 @@ function calcularTotal(cantidad, precio, estado=""){
   });
 })*/
 //Tercer commit, calcular el precio con impuesto
-describe("calcular", () => {
+/*describe("calcular", () => {
   it("deberia calcular ", () => {
     expect(calcularTotal(3, 2,"TX")).toEqual("El total es: 6, el impuesto del estado TX es: 0.0625 y el precio con impuesto es: 6.375");
   });
-})
-
-  
+})*/
+//Cuarto commit, calcular el descuento
+describe("calcular", () => {
+  it("deberia calcular ", () => {
+    expect(calcularTotal(3, 2,"TX")).toEqual("El total es: 6, el impuesto del estado TX es: 0.0625, el precio con impuesto es: 6.375 y usted tiene un descuento de: 0");
+  });
+});
